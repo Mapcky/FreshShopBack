4ccfbf937b1b76227c1c0d6b37eccfcf79678832
Mapcky <mperano625@gmail.com>
2025-06-17 14:20:24 -0300

    default Address Route

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -7,7 +7,7 @@ exports.setNewAddress = async (req, res) => {
 
     const { street, city, state, country, zip, isDefault } = req.body
 
-    const transaction = await models.Order.sequelize.transaction()
+    const transaction = await models.Address.sequelize.transaction()
 
     try {
 
@@ -65,6 +65,42 @@ exports.getAddresses = async (req, res) => {
 
 
     } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Internal server error.", success: false })
+    }
+}
+
+
+exports.changeDefaultAddress = async (req, res) => {
+
+    const { addressId } = req.body
+
+    const transaction = await models.Address.sequelize.transaction()
+
+    try {
+
+        await models.Address.update(
+            { isDefault: false },
+            { where: { user_id: req.userId }, transaction: transaction }
+        )
+
+        const [updatedRows] = await models.Address.update(
+            { isDefault: true },
+            { where: { user_id: req.userId, id: addressId }, transaction }
+        )
+
+        if (updatedRows === 0) {
+            throw new Error("No matching address found or user unauthorized")
+        }
+
+
+        await transaction.commit()
+
+        res.status(200).json({ message: "Default Address Updated Succesfully", success: true })
+
+
+    } catch (error) {
+        await transaction.rollback()
         console.error(error)
         res.status(500).json({ message: "Internal server error.", success: false })
     }
--- routes/address.js
+++ routes/address.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 router.post('/setNew',addressController.setNewAddress)
 router.get('/getAll',addressController.getAddresses)
+router.post('/changeDefault', addressController.changeDefaultAddress)
 
 
 module.exports = router
